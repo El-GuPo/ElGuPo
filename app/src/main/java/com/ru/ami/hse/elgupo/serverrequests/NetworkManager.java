363c1fe723952d7362341cb204bac3dbdb46db21
mrnchk <143181833+mrnchk@users.noreply.github.com>
2025-04-12 00:11:58 +0300

    Update NetworkManager.java

--- app/src/main/java/com/ru/ami/hse/elgupo/serverrequests/NetworkManager.java
+++ app/src/main/java/com/ru/ami/hse/elgupo/serverrequests/NetworkManager.java
@@ -8,16 +8,24 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class NetworkManager {
 
 //    private static final String BASE_URL = "http://localhost:8080/";
-private static final String BASE_URL = "http://10.0.2.2:8080/";
+    private static final String BASE_URL = "http://10.0.2.2:8080/";
     private static NetworkManager instance = null;
     private final PlacesNearbyApiService apiService;
 
     private NetworkManager() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(okHttpClient) // <- подключаем кастомный клиент
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-
+    
         apiService = retrofit.create(PlacesNearbyApiService.class);
     }
 
